545d4c47bcdb2e87e7718374b632de89a350deda
Abraham Martinez Hernandez <abrah@MacBook-Air-2.local>
2025-10-27 21:19:19 -0600

    Primer commit de mi proyecto

---
+++ World  Cup/World  Cup/World__CupApp.swift
@@ -0,0 +1,17 @@
+//
+//  World__CupApp.swift
+//  World  Cup
+//
+//  Created by Abraham Martinez Hernandez on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct World__CupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
